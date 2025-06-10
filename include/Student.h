1f4da8c3a4a4ec7631d063125809d148b021f9fe
cdoggxx <connormon10@icloud.com>
2025-06-10 10:40:53 -0400

    Created header file for Student.h

--- include/Student.h
+++ include/Student.h
@@ -4,11 +4,26 @@
 
 #ifndef STUDENT_H
 #define STUDENT_H
+#include <string>
 
 
 
 class Student {
+private:
+    std::string FirstName;
+    std::string LastName;
+    std::string M_Number;
+    double gpa;
+    int year, month, day;
+public:
+    Student(std::string FirstName, std::string LastName, std::string M_Number, int year, int month, int day, double gpa = 0.0);
 
+    std::string getName();
+    std::string GetM_Number();
+    double getAge();
+    bool operator<(const std::string M_num) const;
+    bool operator>(const std::string M_num) const;
+    bool operator==(const std::string M_num) const;
 };
 
 
