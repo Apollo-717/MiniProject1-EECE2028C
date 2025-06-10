307a4f7471214dd1f0be75d651b28e86daa98a7f
Apollo-717 <Clarkbenjamin717@gmail.com>
2025-06-10 11:23:05 -0400

    DoubleyLinkedList.h fully set up

--- include/DoubleyLinkedList.h
+++ include/DoubleyLinkedList.h
@@ -4,10 +4,46 @@
 
 #ifndef DOUBLEYLINKEDLIST_H
 #define DOUBLEYLINKEDLIST_H
-
+#include "Student.h"
 
 
 class DoubleyLinkedList {
+    public:
+    //constructor
+    DoubleyLinkedList();
+
+    //Method to add an item
+    void AddItem(int index=-1, Student* student);
+
+    //Method to get an item and remove it from the list
+    Student* GetItem(int MNumber);
+
+    //Method to check if item is in list
+    bool IsInList(int MNumber);
+
+    //Method to check if list is empty
+    bool IsEmpty();
+
+    //method to get size
+    int Size();
+
+    //method to track and see next item in list
+    Student* SeeNext();
+
+    //method to find an item at a location in the list, and return the item without removing
+    Student* SeeAt(int index);
+
+    //method to reset SeeNext to beginning
+    void Reset();
+
+    //Destructor
+    ~DoubleyLinkedList();
+
+
+
+    private:
+    int size;
+
 
 };
 
